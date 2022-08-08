b2e55a07503b80ea685f36068cdc2dfcf8def2d0
Mihir Singh <44063783+mihirs16@users.noreply.github.com>
2022-08-08 22:01:53 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,6 @@ func main () {
     
     // repository operations
     repoMetadata := repository.LoadRepository(os.Getenv("MEISTER_PATH"));
-    repoMetadata.PullRepository();
-    // repoMetadata.UpdateSubmodules();
+    // repoMetadata.PullRepository();
+    repoMetadata.UpdateSubmodules();
 }
