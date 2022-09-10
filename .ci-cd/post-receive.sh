c1865513d730756153824cd6c5074e68dd49d158
mihirs16 <mihirs16@gmail.com>
2022-09-10 11:44:22 +0530

    fix: docker exec syntax

--- .ci-cd/post-receive.sh
+++ .ci-cd/post-receive.sh
@@ -2,6 +2,6 @@
 
 docker compose build --no-cache
 docker compose up --detach
-docker compose nginx certbot --non-interactive --nginx -d projects.mihirsingh.dev -m mihirs16@gmail.com --agree-tos
-docker compose nginx certbot renew --dry-run
+docker compose exec nginx certbot --non-interactive --nginx -d projects.mihirsingh.dev -m mihirs16@gmail.com --agree-tos
+docker compose exec nginx certbot renew --dry-run
 
